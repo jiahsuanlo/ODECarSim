6db06dec901a7d1eed87e952e697a1f25db19453
jiahsuanlo <joshloqs@gmail.com>
2015-11-20 16:53:34 -0500

    - add space organization: vehicleSpace and groundSpace belongs   to top-level space - use space-to-space contact to setup collision model - tweak the suspension kps and kds - tweak the ground contact parameters - tweak manual controls - reorganize model script in main function

--- main.cpp
+++ main.cpp
@@ -13,22 +13,24 @@
 
 static dWorldID world;
 static dSpaceID space;
+static dSpaceID vehicleSpace;
 static dSpaceID groundSpace;
 static dJointGroupID contactGroup;
 
 // define body type
-vmCar car(world,space);
+vmCar *car;
 dsFunctions fn;
 
 // define some constants
 dReal STEPSIZE= 0.01;
 
-// obstacle balls
+// ground and obstacle balls
+dGeomID ground;
 const int nObs= 250;
 sphere ball[nObs];
 
 // chassis parameters
-dReal chassisMass= 3000;
+dReal chassisMass= 1000;
 dReal chassisLength= 2.0;
 dReal chassisWidth= 1.5;
 dReal chassisHeight= 0.75;
@@ -37,8 +39,8 @@ dReal chassisHeight= 0.75;
 dReal wheelMass= 10;
 dReal wheelRadius= 0.3;  // wheel radius
 dReal wheelWidth= 0.2;  // wheel width
-dReal kps= 10000.0;  // suspension stiffness
-dReal kds= 1000.0;   // suspension damping
+dReal kps= 100000.0;  // suspension stiffness
+dReal kds= 8000.0;   // suspension damping
 
 // control parameters
 dReal speed= 0.0;
@@ -55,27 +57,38 @@ static void nearCallback(void *data, dGeomID g1, dGeomID g2)
 {
     static int maxContacts= 10;
 
-    // check contacts
-    dContact contact[maxContacts];
-    int nContact= dCollide(g1,g2,maxContacts,&contact[0].geom,sizeof(dContact));
-
-    // treat contacts
-    if (nContact>0)
+    if(dGeomIsSpace(g1) && dGeomIsSpace(g2))
     {
-        for (int i=0; i<nContact; ++i)
+        // colliding a space with something
+        dSpaceCollide2(g1,g2,data,&nearCallback);
+    }
+    else
+    {
+        // check contacts
+        dContact contact[maxContacts];
+        int nContact= dCollide(g1,g2,maxContacts,&contact[0].geom,sizeof(dContact));
+
+        // treat contacts
+        if (nContact>0)
         {
-            contact[i].surface.mode = dContactApprox1;
-            contact[i].surface.mu = 1.0; //dInfinity;
-            //contact[i].surface.slip1 = 0.001;
-            //contact[i].surface.slip2 = 0.001;
-            //contact[i].surface.soft_erp = 0.8;
-            //contact[i].surface.soft_cfm = 1e-6;
+            for (int i=0; i<nContact; ++i)
+            {
+                contact[i].surface.mode = dContactApprox1_1|dContactApprox1_2
+                        |dContactSoftCFM|dContactSoftERP;
 
-            // build contact joint now
-            dJointID c= dJointCreateContact(world,contactGroup,&contact[i]);
-            dJointAttach(c,dGeomGetBody(contact[i].geom.g1),dGeomGetBody(contact[i].geom.g2));
-            //dJointAttach (c,dGeomGetBody(contact[i].geom.g1),dGeomGetBody(contact[i].geom.g2));
+                contact[i].surface.mu = 1;
+                contact[i].surface.mu2 = 1;
+                //contact[i].surface.slip1 = 1e-10;
+                //contact[i].surface.slip2 = 1e-10;
+                contact[i].surface.soft_erp = 0.01;
+                contact[i].surface.soft_cfm = 1e-5;
 
+                // build contact joint now
+                dJointID c= dJointCreateContact(world,contactGroup,&contact[i]);
+                dJointAttach(c,dGeomGetBody(contact[i].geom.g1),dGeomGetBody(contact[i].geom.g2));
+                //dJointAttach (c,dGeomGetBody(contact[i].geom.g1),dGeomGetBody(contact[i].geom.g2));
+
+            }
         }
     }
 }
@@ -84,23 +97,20 @@ static void nearCallback(void *data, dGeomID g1, dGeomID g2)
 static void simloop(int pause)
 {
 
-    car.simControl(manualYes,brakeYes);
+    // update suspension
+    car->setAllWheelSuspension(STEPSIZE,kps,kds);
+    //set control
+    car->simControl();
 
-    dSpaceCollide2(space,groundSpace,0,&nearCallback);
+    dSpaceCollide(space,0,&nearCallback);
     dWorldStep(world,STEPSIZE);
     // collision setup
     dJointGroupEmpty(contactGroup);
 
-    // draw chassis
-    dsSetColor(1.0,0.0,0.0); // red
-    dsDrawBoxD(dBodyGetPosition(carBody.body),dBodyGetRotation(carBody.body),carBody.sides);
-    // draw wheels
-    dsSetColor(1.0,1.0,0.0);
-    dsDrawCylinderD(dBodyGetPosition(FRWheel.body),dBodyGetRotation(FRWheel.body),FRWheel.length,FRWheel.radius);
-    dsDrawCylinderD(dBodyGetPosition(FLWheel.body),dBodyGetRotation(FRWheel.body),FLWheel.length,FRWheel.radius);
-    dsDrawCylinderD(dBodyGetPosition(RRWheel.body),dBodyGetRotation(RRWheel.body),FRWheel.length,FRWheel.radius);
-    dsDrawCylinderD(dBodyGetPosition(RLWheel.body),dBodyGetRotation(RLWheel.body),FRWheel.length,FRWheel.radius);
+    // draw car now
+    car->simDraw();
 
+    // draw obstacles
     for (int i=0;i<nObs;++i)
     {
         dsDrawSphere(dGeomGetPosition(ball[i].geom),dGeomGetRotation(ball[i].geom),ball[i].radius);
@@ -111,30 +121,36 @@ static void simloop(int pause)
 // manual command
 void manualCmd(int cmd)
 {
-    manualYes= 1;
-    brakeYes= 0;
+    car->manualYes= 1;
+    car->brakeYes= 0;
     switch (cmd)
     {
         case 'i': case 'I':
-            speed+= 0.25*M_PI;
+           car->speed+= 0.25*M_PI;
+           car->steer*= 0.98;
             break;
         case ',': case '<':
-            speed*= 0.1*M_PI;
-            brakeYes = 1;
+            car->speed*= 0.1*M_PI;
+            car->brakeYes = 1;
+            car->steer*= 0.98;
             break;
         case 'k': case 'K':
-            speed-= 0.25*M_PI;
+            car->speed-= 0.25*M_PI;
+            car->steer*= 0.98;
             break;
         case 'j': case 'J':
-            steer-= 5*M_PI/180;
+            car->steer= -40*M_PI/180;
             break;
         case 'l': case 'L':
-            steer+= 5*M_PI/180;
+            car->steer= 40*M_PI/180;
             break;
         default:
-            manualYes = 0;
+            car->manualYes = 0;
+            car->steer*= 0.98;
+            car->speed*= 0.999;
     }
 
+    //car->setInitialControls(steer,speed,steerGain);
 }
 
 // camera setup
@@ -153,18 +169,20 @@ void setDSFunctions()
     fn.start= &start;
     fn.stop= NULL;
     fn.command= &manualCmd;
-    //fn.path_to_textures= "c:/dev/ode-0.13/drawstuff/textures";  // win version
-    fn.path_to_textures= "/home/jlo/ode-0.13/drawstuff/textures";  // linux version
+    fn.path_to_textures= "c:/dev/ode-0.13/drawstuff/textures";  // win version
+    //fn.path_to_textures= "/home/jlo/ode-0.13/drawstuff/textures";  // linux version
 }
 
 // create bodies
 void createCar()
 {
-    car.setChassis(chassisMass,chassisLength,
+    car= new vmCar(world,vehicleSpace);
+    car->setChassis(chassisMass,chassisLength,
                    chassisWidth,chassisHeight);
-    car.setAllWheel(wheelMass,wheelWidth,wheelRadius);
-    car.setCarOnGround(0.0,0.0);
-    car.setAllWheelJoint(kps,kds);
+    car->setAllWheel(wheelMass,wheelWidth,wheelRadius);
+    car->setCarOnGround(0.0,0.0);
+    car->setAllWheelJoint();
+    car->setInitialControls(steer,speed,steerGain);
 }
 
 
@@ -191,7 +209,11 @@ int main (int argc, char **argv)
     // create basis
     world= dWorldCreate();
     space= dHashSpaceCreate(0);
-    groundSpace= dHashSpaceCreate(0);
+    vehicleSpace= dHashSpaceCreate(space);
+    groundSpace= dHashSpaceCreate(space);
+    dSpaceSetSublevel(groundSpace,1);
+    dSpaceSetSublevel(vehicleSpace,1);
+
     contactGroup= dJointGroupCreate(0);
 
     //set gravity
@@ -209,7 +231,10 @@ int main (int argc, char **argv)
     dsSimulationLoop(argc,argv,1000,800,&fn);
 
     dWorldDestroy(world);
+    dSpaceDestroy(space);
     dCloseODE();
+
+    delete car;
 
     return 0;
 }
